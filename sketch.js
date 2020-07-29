7ecef1d56aff608af24b720a1b5459d8e0b02c10
Dash <darsh.raheja4@gmail.com>
2020-07-29 20:12:07 +0530

    Constrained bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain,constrainedlog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,7 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    constrainedlog =  new Log(230,50,200, PI/3)
+    chain = new Chain(bird.body,constrainedlog.body);
 }
 
 function draw(){
@@ -62,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedlog.display();
+    chain.display();
 }
